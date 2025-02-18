23d7f51b293c090d7e590ab39c24a6bddbd21c44
Carlos Atanes Vences <carlos117g@gmail.com>
2025-02-18 18:54:52 +0000

    Clase con Eric

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	// https://playground.4geeks.com/todo/todos/pruebas_carlos <- POST de tareas
+	// https://playground.4geeks.com/todo/users/pruebas_carlos <- GET de tareas de un usuario
+	// https://playground.4geeks.com/todo/users/pruebas_carlos <- POST de un usuario
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+	/*
+		{
+		"label": "string",
+		"is_done": false
+		}
+	*/
+	const [users, setUser] = useState("ehiber");
+	const [tareas, setTareas] = useState([]);
+	const [inputText, setInputText] = useState("");
+
+
+	async function getTareas() {
+		const response = await fetch("https://playground.4geeks.com/todo/users/"+users);
+		const data = await response.json();
+		setTareas(data.todos);
+	}
+
+	async function addNewTask(e) {
+		e.preventDefault();
+
+		const tarea = {
+			label: inputText,
+			is_done: false
+		}
+
+		const response = await fetch("https://playground.4geeks.com/todo/todos/"+users, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(tarea)
+		})
+
+		
+		setInputText("");
+		getTareas();
+	}
+
+	async function generateUser() {
+		const responseGet = await fetch("https://playground.4geeks.com/todo/users/"+users);
+		if (!responseGet.ok) {
+			const response = await fetch("https://playground.4geeks.com/todo/users/"+users, {
+				method: "POST"
+			});
+			const data = await response.json()
+			console.log(data);
+        }
+	}
+
+	useEffect(() => {
+		getTareas();
+		generateUser();
+	}, []);
+
+	return (
+		<>
+			<form onSubmit={(e) => addNewTask(e)}>
+				<input value={inputText} onChange={(e) => setInputText(e.target.value)} placeholder="Regar las plantas" />
+			</form>
+			<ul>
+				{tareas.map((tarea, index) => (
+					<li key={index}>{tarea.label}</li>
+				))}
+			</ul>
+		</>
 	);
 };
 
